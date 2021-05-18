c83bc2e11fa5f351a95fc4c3a0035b03486419dc
vvhale <mibarannikov@yandex.ru>
2021-05-18 22:18:27 +0300

    ептеть на не блокирующих с кампареЭндСет

--- src/main/java/stepGo.java
+++ src/main/java/stepGo.java
@@ -1,43 +1,33 @@
 import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicBoolean;
 
 public class stepGo implements Runnable {
     private int numberOfStep;
     private step St;
-    stepGo(int numberOfStep, step Step){
-        this.numberOfStep=numberOfStep;
-        this.St=Step;
+    private boolean paramStep;
+
+    stepGo(int numberOfStep, step Step, boolean paramStep) {
+        this.numberOfStep = numberOfStep;
+        this.St = Step;
+        this.paramStep = paramStep;
     }
 
     @java.lang.Override
     public void run() {
         int i = 0;
-        while(true){
-            if (St.getControllerStep()){
-                St.Right();
-                St.setControllerStep(false);
-                i++;
+        Boolean localAtomBool;
+        while (true) {
+            do {
+                localAtomBool = St.getControllerStep().get();
+                if (localAtomBool == paramStep) {
+                    St.nextStep();
+                    i++;
+                }
+            } while (!St.getControllerStep().compareAndSet(localAtomBool, !paramStep));
+            if (i == numberOfStep) {
+                return;
             }
-            else{
-                St.Left();
-                St.setControllerStep(true);
-                i++;
-            }
-            if (i==St.getNumberOfSteps()){return;}
         }
 
     }
 }
-    /*private step st;
-    private int gS, anGS;
-    stepGo(step Step,int goStep){st=Step;gS=goStep;}//gS какой ногой ходить 0-левая, 1-правая
-    public void run() {
-        int i =0;
-        if (gS==0){anGS=1;} else{anGS=0;}
-        while(true) {
-
-            if (st.getC()==gS){
-                i++;st.Go(gS);st.setC(anGS);}
-            if (i==100){return;}
-      }
-    }*/
-
